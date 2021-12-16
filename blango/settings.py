105588d2307a6e1a1a238df6d4be463a1084643d
Bandapalli Chandrappa <bschandu@smerkato.com>
2021-12-16 08:49:49 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'blog',
     'crispy_forms',
     'crispy_bootstrap5',
+    'rest_framework',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
